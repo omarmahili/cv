9b10c8b28b7e66a0a7f3e6b6b07778d03f14c9b2
omarmahili <omar.mahili@hotmail.it>
2020-10-10 15:28:51 +0200

    Add description prop to Profile component

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -3,17 +3,20 @@ import React from "react";
 interface ProfileProps {
   fullname: string;
   expertise: string;
+  description: string;
 }
 
 const Profile: React.FunctionComponent<ProfileProps> = ({
   fullname,
   expertise,
+  description,
 }) => (
   <div className="flex flex-col items-center">
     <div className="w-40 h-40 rounded-full bg-gray-400"></div>
     <div className="flex flex-col items-center mt-6">
       <p className="text-lg">{fullname}</p>
       <p className="text-xl text-orange-600">{expertise}</p>
+      <p className="text-gray-600 italic text-center mt-6">"{description}"</p>
     </div>
   </div>
 );

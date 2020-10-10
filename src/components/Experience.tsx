cc8eba2fed52911991fe4ecc92056f815ab2e087
omarmahili <omar.mahili@hotmail.it>
2020-10-10 15:27:19 +0200

    Improve component style and render new fields

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import moment from "moment";
 import ExperienceModel from "../models/experience";
 
 interface ExperienceProps {
@@ -8,15 +7,36 @@ interface ExperienceProps {
 
 const Experience: React.FunctionComponent<ExperienceProps> = ({
   experience,
-}) => (
-  <div>
-    <p>{experience.title}</p>
-    <div>
-      <p>{moment(experience.from).format("DD/MM/YYYY")}</p>
-      <p>{experience.to ? moment(experience.to).format("DD/MM/YYYY") : 'Current'}</p>
-    </div>
-    <p>{experience.description}</p>
-  </div>
-);
+}) => {
+  const companyOrInstitute = experience.companyOrInstitute ? (
+    <p className="text-base text-gray-800">{`at ${experience.companyOrInstitute}`}</p>
+  ) : null;
+
+  const description = experience.description ? (
+    <p className="text-sm my-2">{experience.description}</p>
+  ) : null;
+
+  const profession = experience.profession ? (
+    <p className="text-lg text-orange-600">{experience.profession}</p>
+  ) : null;
+
+  return (
+    <li className="flex flex-row text-sm text-gray-800">
+      <div className="h-6 flex justify-center items-center">
+        <div className="w-2 h-2 border-2 border-blue-600 rounded-full"></div>
+      </div>
+      <div className="flex flex-col ml-4">
+        <p className="h-6 flex items-center">{`${experience.from} - ${
+          experience.to || "Current"
+        }`}</p>
+        <div className="mt-2">
+          {profession}
+          {companyOrInstitute}
+          {description}
+        </div>
+      </div>
+    </li>
+  );
+};
 
 export default Experience;

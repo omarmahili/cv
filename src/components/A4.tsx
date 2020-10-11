d69a9fb93c3af01ff52e806a30364838624c6dc6
omarmahili <omar.mahili@hotmail.it>
2020-10-11 11:03:53 +0200

    Format components

--- src/components/A4.tsx
+++ src/components/A4.tsx
@@ -4,14 +4,8 @@ interface A4Props {
   children: React.ReactNode;
 }
 
-const A4: React.FunctionComponent<A4Props> = ({ children }) => {
-  return (
-    <div
-      className="a4 bg-white shadow flex flex-row"
-    >
-      {children}
-    </div>
-  );
-};
+const A4: React.FunctionComponent<A4Props> = ({ children }) => (
+  <div className="a4 bg-white shadow flex flex-row">{children}</div>
+);
 
 export default A4;
--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,14 +1,12 @@
 import React from "react";
 
 interface TagProps {
-    children: string;
+  children: string;
 }
 
-const Tag: React.FunctionComponent<TagProps> = ({
-    children,
-}) => (
+const Tag: React.FunctionComponent<TagProps> = ({ children }) => (
   <div className="bg-blue-400 px-2 py-1 shadow mx-1 my-1">
-      <p className="text-white text-xs">{children}</p>
+    <p className="text-white text-xs">{children}</p>
   </div>
 );
 

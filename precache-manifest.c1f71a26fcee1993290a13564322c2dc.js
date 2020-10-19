9224ab7027ffc20529e34f890818d88dc27b03d3
omarmahili <omar.mahili@hotmail.it>
2020-10-19 22:59:43 +0200

    Updates

---
+++ precache-manifest.c1f71a26fcee1993290a13564322c2dc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d54c7db5168b26117b93baf7511232c6",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "a60bf18b0bc00a6a8c90",
+    "url": "/cv/static/css/main.2b013fc0.chunk.css"
+  },
+  {
+    "revision": "53d22ea9e6d7bdf6302b",
+    "url": "/cv/static/js/2.7ad9e742.chunk.js"
+  },
+  {
+    "revision": "7ec01595672f75e83fd81b41f132f4c1",
+    "url": "/cv/static/js/2.7ad9e742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33e4d0d01cc6edb50af",
+    "url": "/cv/static/js/3.1fa4b909.chunk.js"
+  },
+  {
+    "revision": "b51ed37b171a34c94200ff01ec170480",
+    "url": "/cv/static/js/3.1fa4b909.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f99a33b6adc2099db111",
+    "url": "/cv/static/js/4.4c2c5108.chunk.js"
+  },
+  {
+    "revision": "0512cf1466087f5c44c5c61e4b32a001",
+    "url": "/cv/static/js/4.4c2c5108.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a60bf18b0bc00a6a8c90",
+    "url": "/cv/static/js/main.e5bbcbcd.chunk.js"
+  },
+  {
+    "revision": "e99c9fb50b83c8f65df2",
+    "url": "/cv/static/js/runtime-main.e688e391.js"
+  },
+  {
+    "revision": "37c8a1aaa2c1891aa0960ff03c377649",
+    "url": "/cv/static/media/Questrial-Regular.37c8a1aa.ttf"
+  }
+]);
